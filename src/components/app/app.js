7867313cf7670177ab597165b57764ddb3174662
Сергей Килочицкий <kila.sergey1@gmail.com>
2019-10-30 23:52:31 +0300

    starting settings

---
+++ src/components/app/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App=()=>{
+	return(
+		<div>
+			1212
+		</div>
+	)
+}
+
+export default App;
---
+++ src/services/test-service.js
@@ -0,0 +1,14 @@
+export default class TestService{
+
+	_apiBase='https://jsonplaceholder.typicode.com';
+
+	getResourse=async (url)=>{
+		const response= await fetch(`${this._apiBase}${url}`);
+		if(!response.ok){
+			throw new Error(`Can't fetch ${url}, received ${response.status}`)
+		}
+		return response.json()
+	}
+
+}
+
